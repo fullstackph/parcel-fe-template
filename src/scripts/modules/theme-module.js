5d9d27bab36b27f810c2ccdbeb0aaed5fdcee7b4
taze-fullstack <taze@fullstack.ph>
2019-02-26 17:30:05 +0800

    Explicit import of some dependencies that rely on window

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -1,13 +1,17 @@
 AppName.Modules.ThemeModule = (function() {
   //Dependencies
-  var core = AppName.Core;
+  // var core = AppName.Core;
+  // var modules = AppName.Modules;
+
+  window.$ = require('jquery');
+  // var Swiper = require('swiper').default;
 
   //////////////////////
   // Private Methods //
   ////////////////////
   var _privateMethod = function() {
     // private stuff
-    console.log('init of theme-module.js')
+    // console.log('init of theme-module.js')
   };
 
   /////////////////////
